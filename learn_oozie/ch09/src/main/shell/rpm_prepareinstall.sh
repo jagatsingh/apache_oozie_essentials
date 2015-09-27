eae82fc32f61a12fca843c87453ad1adab519ddf
Jagat Singh <jagatsingh@gmail.com>
2015-09-27 15:02:48 +1000

    Chapter 9 changes

--- learn_oozie/ch09/src/main/shell/rpm_prepareinstall.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-echo "Checking environment to install Oozie"
-
-[ -z "$OOZIE_CODE_ENV" ] && echo "Please set some environment variable to install (example export OOZIE_CODE_ENV=dev" && exit 1;
